6d55aab98d473eb2370eca3afc62dbb1ce658acb
PauloSarmento <paulo.cesar.sarmento@hotmail.com>
2023-11-07 10:29:36 -0300

    first commit

---
+++ docker-entrypoint-initdb.d/01-create-table copy.sql
@@ -0,0 +1,10 @@
+-- Use o banco de dados 'nodedb'
+USE nodedb;
+
+-- Crie a tabela 'people' se não existir
+CREATE TABLE IF NOT EXISTS people (
+  id INT NOT NULL AUTO_INCREMENT,
+  name VARCHAR(255),
+  PRIMARY KEY (id)
+);
+
---
+++ node/index.js
@@ -0,0 +1,32 @@
+const express = require("express");
+const app = express();
+const port = 3000;
+const config = {
+  host: "db",
+  user: "root",
+  password: "root",
+  database: "nodedb",
+};
+const mysql = require("mysql");
+const connection = mysql.createConnection(config);
+const sql = `INSERT INTO people(name) values('Paulo')`;
+connection.query(sql);
+
+app.get("/", (req, res) => {
+  // Consulta o banco de dados para obter as pessoas cadastradas
+  connection.query("SELECT * FROM people", (err, result) => {
+    if (err) {
+      console.error(err);
+      res.status(500).send("Erro ao buscar pessoas cadastradas.");
+      return;
+    }
+
+    const people = result.map((row) => row.name);
+    const html = `<h1>Full Cycle Rocks!</h1><p>${people.join(", ")}</p>`;
+    res.send(html);
+  });
+});
+
+app.listen(port, () => {
+  console.log("Rodando na porta " + port);
+});
